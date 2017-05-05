059cd340615b3bc0ed427a2ae2e9bd594a13168c
hidalgo <hidalgochpnce@gmail.com>
2017-05-05 16:03:26 -0500

    layer slider update

--- resources/assets/js/vendors/function.js
+++ resources/assets/js/vendors/function.js
@@ -1,3 +1,74 @@
 /**
  * Created by Sigma on 12/04/2017.
  */
+$(document).ready(function(){
+    $(".dropdown").hover(
+        function() {
+            $('.dropdown-menu', this).fadeIn("fast").not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');
+        },
+        function() {
+            $('.dropdown-menu', this).fadeOut("fast").not('.in .dropdown-menu').stop(true,true).slideUp("400").fadeOut("fast");
+            $(this).toggleClass('open');
+        }
+    );
+     // carousel
+    $('.owl-carousel ').owlCarousel({
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        items:1,
+        // margin:30,
+        // stagePadding:30,
+        smartSpeed:450,
+        autoplay:true,
+        autoplayTimeout:15000,
+        loop:true,
+    });
+
+
+
+});
+
+(function($) {
+
+    $.fn.parallax = function(options) {
+
+        var windowHeight = $(window).height();
+
+        // Establish default settings
+        var settings = $.extend({
+            speed        : 0.15
+        }, options);
+
+        // Iterate over each object in collection
+        return this.each( function() {
+
+            // Save a reference to the element
+            var $this = $(this);
+
+            // Set up Scroll Handler
+            $(document).scroll(function(){
+
+                var scrollTop = $(window).scrollTop();
+                var offset = $this.offset().top;
+                var height = $this.outerHeight();
+
+                // Check if above or below viewport
+                if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+                    return;
+                }
+
+                var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
+
+                // Apply the Y Background Position to Set the Parallax Effect
+                $this.css('background-position', 'center ' + yBgPosition + 'px');
+
+            });
+        });
+    }
+}(jQuery));
+
+$('.bg-1').parallax({
+    speed :	.03,
+
+});
--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -16,19 +16,36 @@
 </head>
 <body>
 
-    <header class="">
-        <div class="">
-            <div class="container clearfix">
-                <div class="row">
-                    <div class="row col-lg-3 col-md-3 col-sm-4 col-xs-12">
-                        <a class="margin-top-10 margin-bottom-30 margin-top-20" href="/"><img src="https://gotoperu.com/img/logos/logo-gotoperu.png" title="gotoperu" alt="logo gotoperu" class="img-responsive"></a>
+    {{--<header>--}}
+        {{--<div class="container">--}}
+            {{--<div class="row">--}}
+                {{--<div class="col-lg-3">--}}
+                    {{--<a href=""><img src="{{asset('images/logos/logo-gotoperu.png')}}" title="gotoperu" alt="logo gotoperu" class="img-responsive"></a>--}}
+                {{--</div>--}}
+                {{--<div class="col-lg-2">--}}
+                    {{--<a href="http://www.asta.org/MemberServices/MemberDirectoryIndivdualDisplayASTA.cfm?navItemNumber=11304&amp;id=900235751" target="_blank"><img src="https://gotoperu.com/img/logos/asta-logo.png" alt="logo asta" class="img-responsive"></a>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</header>--}}
+
+    <header class="font-montserrat">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+                    <div class="margin-top-22">
+                        <a href="/"><img src="{{asset('images/logos/logo-gotoperu.png')}}" title="gotoperu" alt="logo gotoperu" class="img-responsive"></a>
                     </div>
-                    <div class="col-lg-2 col-sm-3 text-center hidden-md hidden-xs">
-                        <a href="http://www.asta.org/MemberServices/MemberDirectoryIndivdualDisplayASTA.cfm?navItemNumber=11304&amp;id=900235751" target="_blank"><img src="https://gotoperu.com/img/logos/asta-logo.png" alt="logo asta"></a>
+                </div>
+                <div class="col-lg-2 col-sm-3 hidden-md hidden-xs">
+                    <div class="text-center padding-5">
+                        <a href="http://www.asta.org/MemberServices/MemberDirectoryIndivdualDisplayASTA.cfm?navItemNumber=11304&amp;id=900235751" target="_blank"><img src="{{asset('images/logos/logo-asta.png')}}" alt="logo asta" class="img-responsive"></a>
                     </div>
-                    <div class="row header-social col-lg-4 col-md-5 col-xs-12 margin-top-10 text-center hidden-sm">
-                        <h6 class="color-black-1 contact-info-2 text-center margin-bottom-0">BEST TESTIMONIALS <span class="color-green-1">&amp; LOCAL CONNECTIONS</span></h6>
-                        <div class="row text-center languaje">
+                </div>
+                <div class="col-lg-4 col-md-5 col-xs-12 hidden-sm">
+                    <div class="text-center margin-top-22">
+                        <b class="text-15">BEST TESTIMONIALS <span class="color-goto-green">&amp; LOCAL CONNECTIONS</span></b>
+                        <div class="text-center text-12 hidden-xs">
                             <a href="https://gotoperu.com/">English |</a>
                             <a rel="alternate" hreflang="es" href="https://gotoperu.com/es/" target="_blank">Espa&ntilde;ol |</a>
                             <a rel="alternate" hreflang="fr" href="http://www.andesperou.com/" target="_blank">Fran&ccedil;ais |</a>
@@ -37,20 +54,72 @@
                             <a rel="alternate" hreflang="es" href="https://gotoperu.com/ru/" target="_blank">Pусский</a>
                         </div>
                     </div>
-                    <div class="header-social col-lg-2 col-md-3 col-sm-3 col-xs-12 margin-top-20">
-                        <div class="row text-center">
-                            <h6 class="color-orange-2 contact-info-2 header-small-text-2">(813) 600-3042</h6>
-                        </div>
-                    </div>
-                    <div class="header-social padding-left-0 padding-right-0 col-lg-1 col-md-1 col-sm-2 hidden-xs margin-top-30">
-                        <div class="row text-left">
-                            <a href="#" data-toggle="modal" data-target="#contact-us" class="btn-availability header-xs-text-2"><b>CONTACT US</b></a>
-                        </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-5 col-xs-12">
+                    <div class="text-center margin-top-22">
+                        <b class="color-goto-orange"><span class="text-18">(813) 600-3042</span> <a href="" class="btn btn-warning">CONTACT US</a></b>
                     </div>
                 </div>
             </div>
         </div>
     </header><!-- /.item -->
+
+
+    <div class="owl-carousel owl-theme">
+        <div class="item">
+            <img src="{{asset('images/slider/machupicchu.jpg')}}" alt="">
+        </div>
+        <div class="item">
+            <img src="{{asset('images/slider/machupicchu.jpg')}}" alt="">
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <hr>
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h3>Hello.</h3></div>
+                    <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate.
+                        Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis
+                        dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan.
+                        Aliquam in felis sit amet augue.
+                    </div>
+                    <div class="panel-footer text-right"><a href="#" class="btn btn-default">More</a></div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h3>Hello.</h3></div>
+                    <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate.
+                        Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis
+                        dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan.
+                        Aliquam in felis sit amet augue.
+                    </div>
+                    <div class="panel-footer text-right"><a href="#" class="btn btn-default">More</a></div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h3>Hello.</h3></div>
+                    <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate.
+                        Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis
+                        dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan.
+                        Aliquam in felis sit amet augue.
+                    </div>
+                    <div class="panel-footer text-right"><a href="#" class="btn btn-default">More</a></div>
+                </div>
+            </div>
+        </div><!--/row-->
+
+        <hr>
+
+    </div><!--/container-->
+
+
 
     {{--scripts--}}
     <script>
