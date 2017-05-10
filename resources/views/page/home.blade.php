18301e8287b62664bf3ef4b8215e105082b922b4
hidalgo <hidalgochpnce@gmail.com>
2017-05-10 17:00:27 -0500

    Home page con banner left

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -26,11 +26,11 @@
 <nav class="cd-3d-nav-container">
     <ul class="cd-3d-nav no-padding">
         <li class="cd-selected">
-            <a href="#0">Tour Packages</a>
+            <a href="#cd-placeholder-1">Tour Packages</a>
         </li>
 
         <li>
-            <a href="#0">Destinations</a>
+            <a href="#cd-placeholder-1">Destinations</a>
         </li>
 
         <li>
@@ -50,18 +50,105 @@
 </nav> <!-- .cd-3d-nav-container -->
 
 <div id="cd-intro">
-    <div class="container">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-            <div class="padding-10">
-                <img src="{{asset('images/logos/logo-gotoperu-ave.png')}}" alt="" class="img-responsive">
+    <div class="banner-header color-white">
+        <div class="banner-header-top text-center font-montserrat">
+            <p class="text-20 color-white text-center">Peru & Machupicchu <span class="display-block">(3 days)</span></p>
+            <div class="list-services text-center">
+                <ul class="no-padding margin-bottom-0">
+                    <li>
+                        <img src="{{asset('images/icons/include/assistances.png')}}" alt="" class="img-responsive">
+                    </li>
+                    <li>
+                        <img src="{{asset('images/icons/include/breakfast.png')}}" alt="" class="img-responsive">
+                    </li>
+                    <li>
+                        <img src="{{asset('images/icons/include/entrances.png')}}" alt="" class="img-responsive">
+                    </li>
+                    <li>
+                        <img src="{{asset('images/icons/include/flight.png')}}" alt="" class="img-responsive">
+                    </li>
+                    <li>
+                        <img src="{{asset('images/icons/include/hotels.png')}}" alt="" class="img-responsive">
+                    </li>
+                    <li>
+                        <img src="{{asset('images/icons/include/tours.png')}}" alt="" class="img-responsive">
+                    </li>
+                    <li>
+                        <img src="{{asset('images/icons/include/trains.png')}}" alt="" class="img-responsive">
+                    </li>
+                    <li>
+                        <img src="{{asset('images/icons/include/transfers.png')}}" alt="" class="img-responsive">
+                    </li>
+                </ul>
+            </div>
+            <p class="text-18 text-center">$1500 with flights <span class="text-12 display-block">$891 Ground Package</span></p>
+            <a href="" class="btn btn-sm btn-success ">View Trip</a>
+        </div>
+        <div class="divider margin-top-20"></div>
+        <div class="banner-header-bottom">
+            <h4 class="text-16 color-goto-orange margin-bottom-5"><b>Daily Departures</b></h4>
+            <p>We have groups starting every ?? Cusco Machu Picchu Lima</p>
+            <h4 class="text-16 color-goto-orange margin-bottom-5"><b>Local Operator</b></h4>
+            <p>We live and work in Peru that also allow to offer you the best</p>
+            <h4 class="text-16 color-goto-orange margin-bottom-5"><b>Daily Departures</b></h4>
+            <p>For our travellers and our communities <a href="" class="btn-link">View last activities</a></p>
+
+        </div>
+    </div>
+    <div class="container-fluid">
+        {{--<div class="col-md-5">--}}
+            {{--<div class="margin-top-10">--}}
+                {{--<a href="">--}}
+                    {{--<div class="col-md-2 no-padding">--}}
+                        {{--<img src="{{asset('images/logos/logo-ave-gotoperu.png')}}" alt="" class="img-responsive">--}}
+                    {{--</div>--}}
+                    {{--<div class="col-md-10 no-padding">--}}
+                        {{--<div class="margin-top-10">--}}
+                            {{--<img src="{{asset('images/logos/logo-gotoperu.png')}}" alt="" class="img-responsive">--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</a>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        <div class="col-md-7">
+            {{--<div class="margin-top-30">--}}
+                {{--<b class="text-30">...once in your lifetime!</b>--}}
+            {{--</div>--}}
+            <div class="margin-top-20">
+                <img src="{{asset('images/logos/logo-gotoperu-2.png')}}" alt="" class="img-responsive">
             </div>
         </div>
-        <div class="col-md-4"></div>
+        {{--<div class="col-md-2">--}}
+            {{--<div class="padding-10">--}}
+                {{--<img src="{{asset('images/logos/logo-gotoperu-ave.png')}}" alt="" class="img-responsive">--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        <div class="col-md-5">
+            <div class="text-right font-montserrat margin-top-10">
+                <b class="text-30 margin-right-30">(813) 454-9707</b>
+            </div>
+        </div>
     </div>
-    <div id="cd-intro-tagline">
+    <div id="cd-intro-tagline" class="font-montserrat clearfix">
         {{--<h1>Secondary Fixed Navigation</h1>--}}
-        <a href="http://codyhouse.co/?p=296" class="cd-btn">Inquire Now</a>
+
+        <div class="col-md-6 col-md-offset-5  margin-top-85">
+            <div class="padding-10">
+                <div class="col-md-6">
+                    <div class="box-header-action thumbnail padding-10">
+                        <p class="text-20 no-margin">Customize</p>
+                        <a href="" class="cd-btn">Inquire Now</a>
+                    </div>
+                </div>
+                <div class="col-md-6 ">
+                    <div class="box-header-action thumbnail padding-10">
+                        <p class="text-20 no-margin">Book Today</p>
+                        <a href="" class="cd-btn-header bg-goto-green">View Offers</a>
+                    </div>
+                </div>
+            </div>
+            {{--<a href="" class="cd-btn">Inquire Now</a>--}}
+        </div>
         {{--<a href="http://codyhouse.co/?p=296" class="cd-btn">View Offers</a>--}}
         {{--<a href="#0" id="cd-logo"><img src="{{asset('images/cd-logo.svg')}}" alt="Logo"></a>--}}
     </div> <!-- #cd-intro-tagline -->
@@ -104,7 +191,34 @@
         </ul>
     </nav>
 </div> <!-- .cd-secondary-nav -->
-<div class="cd-main-content">
+
+<div class="container">
+    <div class="spacer-10"><!--.row -->
+
+        <div class="col-lg-2 col-md-2 col-sm-2 hidden-xs"><a href="https://www.facebook.com/GOTOPERUcom/" target="_blank"><img src="{{asset('images/logos/logo-facebook.png')}}" title="gotoperu facebook 30k like" alt="gotoperu facebook 30k like"></a></div>
+
+        <div class="col-lg-2 col-md-2 col-sm-2 hidden-xs"><a href="http://www.asta.org/MemberServices/MemberDirectoryIndivdualDisplayASTA.cfm?navItemNumber=11304&amp;id=900235751" target="_blank"><img src="{{asset('images/logos/logo-asta-grey.png')}}" title="asta" alt="logo asta"></a></div>
+
+        <div class="col-lg-2 col-md-2 col-sm-2 hidden-xs"><a href="http://investors.nytco.com/press/press-releases/press-release-details/2014/The-New-York-Times-Travel-Show-Announces-Special-Travel-Discounts-for-Show-Attendees/default.aspx" target="_blank"><img src="{{asset('images/logos/logo-newyorktimes.png')}}" title="the new york times" alt="the new york times"></a></div>
+
+        <div class="col-lg-2 col-md-2 col-sm-2 hidden-xs"><a href="http://apoturperu.org/" target="_blank"><img src="{{asset('images/logos/logo-apotur.png')}}" title="apotur" alt="logo apotur"></a></div>
+
+        <div class="col-lg-2 col-md-2 col-sm-2 hidden-xs"><a href="http://www.peru.travel/en-us/Search-Travel-Services/pageindex/0/categoryid/5/subcategoryidl1/-1/subcategoryidl2/-1.aspx?keyword=go+to+peru&amp;destid=,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25" target="_blank"><img src="{{asset('images/logos/logo-promperu.png')}}" title="promperu" alt="logo promperu"></a></div>
+
+        <div class="col-lg-2 col-md-2 col-sm-2 hidden-xs"><a href="#" target="_blank"><img src="{{asset('images/logos/logo-meetup.png')}}" title="meetup" alt="logo meetup"></a></div>
+
+    </div>
+</div>
+
+    <div class="container-fluid">
+        <div class="col-md-12">
+            <h1>PERU TRAVEL PACKAGES</h1>
+            <h2>GREAT ADVENTURE PACKAGES FOR EVERYONE WITH GOTOPERU</h2>
+            <p>Offering you an authentic and reliable travel experience in peru welcoming you to explore the many fully customized travel packages we have that suit every budget. let us plan your unforgettable trip to our majestic machu picchu and the rest of our homeland… peru!</p>
+        </div>
+    </div>
+
+<div class="">
     <section id="cd-placeholder-1" class="cd-section cd-container">
         <h2>TOUR PACKAGES</h2>
         <p>
@@ -112,7 +226,7 @@
         </p>
     </section> <!-- #cd-placeholder-1 -->
 
-    <section id="cd-placeholder-2" class="cd-section cd-container">
+    <section id="cd-placeholder-2" class="cd-section">
         <h2>HOTELS</h2>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ab consequatur fugiat delectus sint in velit quo possimus, ullam aspernatur ipsa natus tenetur nulla distinctio doloremque totam. Earum enim sed dolorum, exercitationem temporibus quaerat eos, accusantium amet facilis facere eaque commodi optio quidem rem minima nisi laborum quae animi nostrum aut voluptates veniam. Cum neque quam fuga sapiente quidem eum necessitatibus nulla, cupiditate a, repudiandae iusto in dolor eaque commodi nostrum consequuntur dolores velit eligendi dolorem quae. Distinctio quae, cumque aliquid quos consequuntur perspiciatis voluptates, laboriosam velit qui et aut sint esse nemo voluptatibus, dolore veritatis natus facilis commodi sed. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat omnis quia maiores, et. Aut facilis ducimus, labore culpa, nemo eum qui quae, rem libero aspernatur, inventore quos modi a ex sit. Nulla iure odio at quo corporis assumenda ea fugiat adipisci aspernatur incidunt sapiente consequatur, ullam cumque dolorem facilis laboriosam a dolore similique velit esse. Eveniet omnis voluptas, nobis dicta cupiditate odit, similique mollitia eos dignissimos provident voluptatem adipisci ullam facilis alias! Reprehenderit voluptatem provident ut veniam aliquid, labore culpa.
